85ee8358eec0ebb5f759c95239c1ed96eaaa8d9f
thomas-legris-idnow <162141739+thomas-legris-idnow@users.noreply.github.com>
2025-06-04 11:01:21 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IDnowEID",
-            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.0.1/IDnowEID.xcframework.zip",
- checksum: "24175229f308f76e931bdcb1399a0dd751389338fb3030617de4306cb5c8b734
-"
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.0.1/IDnowEID.xcframework.zip", 
+            checksum: "24175229f308f76e931bdcb1399a0dd751389338fb3030617de4306cb5c8b734"
         )
     ]
 )
