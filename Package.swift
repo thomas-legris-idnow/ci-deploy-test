7d1019e478753d346d6c17f81fd3495725f4b7d1
Your Name <you@example.com>
2025-06-04 16:58:30 +0200

    Update the package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IDnowEID",
-            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.0.1/IDnowEID.xcframework.zip", 
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.1.1.testanser/IDnowEID.xcframework.zip",
             checksum: "24175229f308f76e931bdcb1399a0dd751389338fb3030617de4306cb5c8b734"
         )
     ]
