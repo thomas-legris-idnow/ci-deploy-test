2b4e7ae2f08df30b51caf0b994a56cc495809112
eid-developer <eid-developer@idnow.io>
2025-06-05 10:53:37 +0200

    Release - Update SDK to version 1.0.0-test-github-v5

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "IDnowEID",
-            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.0.0-test-github-v4/IDnowEID.xcframework.zip",
-            checksum: "8806df620961ee04b8a15bc5fe01128618683e91fd116fa2e122498971187043"
+            url: "https://github.com/idnow/eid-sdk-ios/releases/download/1.0.0-test-github-v5/IDnowEID.xcframework.zip",
+            checksum: "aa4d2cf2e2a5100578fb7d0a0ae2df1f6b9f94055aa2e502c2a01ffbfb355cfe"
         )
     ]
 )
